2a2e02da4036457eeded69d73fdfde9f9de1d91c
pupil <pupil@ivkhk.ee>
2021-09-08 15:07:24 +0300

    JPTV20GAMEFIRST

---
+++ src/jptv20game/JPTV20GAME.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jptv20game;
+import java.util.Random;
+import java.util.Scanner;
+/**
+ *
+ * @author pupil
+ */
+public class JPTV20GAME {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int number = random.nextInt(10);
+        System.out.println("Отгадай число !");
+         int usernumber = scanner.nextInt();
+        if (number == usernumber){
+            System.out.println("URAAAAAAAA!!!!!!!! POBEDAAA");
+        }
+        else {
+            System.out.println("PROIGRAL!!!!!");
+        }
+    }
+    
+}
