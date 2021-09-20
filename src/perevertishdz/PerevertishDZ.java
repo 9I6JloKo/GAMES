3d40c6fade16fd6efdaafe6d0ff4790ee59ddef2
Max <maksim.grisin@ivkhk.ee>
2021-09-20 16:49:22 +0300

    StringNAOBOROT

---
+++ src/perevertishdz/PerevertishDZ.java
@@ -0,0 +1,18 @@
+package perevertishdz;
+
+import java.util.Scanner;
+
+public class PerevertishDZ {
+
+public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+String firstString = scanner.nextLine();
+
+char[] charFirstString = firstString.toCharArray();
+char[] charSecondString = new char[charFirstString.length];
+for (int i = 0; i < charFirstString.length; i++) {
+charSecondString[(charSecondString.length-1)-i] = charFirstString[i];
+}
+System.out.println(charSecondString);
+}
+}
